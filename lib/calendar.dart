6ac489c4cf21b173edd3972558ff9628fe262c28
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-04-30 00:35:25 +0530

    Calendar done

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -57,111 +57,129 @@ class _CalendarState extends State<Calendar> {
       backgroundColor: Color(0xffedeefc),
       body: Column(
         children: <Widget>[
-          Expanded(
-              child: Column(
-            children: <Widget>[
-              SafeArea(
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.06,
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          SafeArea(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.06,
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
                       children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(left: 5.0),
-                              child: IconButton(
-                                icon: Icon(Icons.chevron_left),
-                                color: Colors.black,
-                                iconSize: 24,
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .popUntil((route) => route.isFirst);
-                                },
-                              ),
-                            ),
-                          ],
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5.0),
+                          child: IconButton(
+                            icon: Icon(Icons.chevron_left),
+                            color: Colors.black,
+                            iconSize: 24,
+                            onPressed: () {
+                              Navigator.of(context)
+                                  .popUntil((route) => route.isFirst);
+                            },
+                          ),
                         ),
-                        Column(
-                          children: <Widget>[
-                            Padding(
-                                padding: const EdgeInsets.only(right: 5.0),
-                                child: IconButton(
-                                  icon: Icon(Icons.add),
-                                  color: Colors.black,
-                                  iconSize: 24,
-                                  onPressed: _showAddDialog,
-                                )),
-                          ],
-                        )
-                      ]),
-                ),
-              ),
-              Column(
-                children: <Widget>[
-                  TableCalendar(
-                    events: _events,
-                    initialCalendarFormat: CalendarFormat.month,
-                    availableGestures: AvailableGestures.horizontalSwipe,
-                    calendarStyle: CalendarStyle(
-                      canEventMarkersOverflow: true,
+                      ],
                     ),
-                    headerStyle: HeaderStyle(
-                        centerHeaderTitle: true, formatButtonVisible: false),
-                    startingDayOfWeek: StartingDayOfWeek.monday,
-                    onDaySelected: (date, events) {
-                      setState(() {
-                        _selectedEvents = events;
-                      });
-                    },
-                    builders: CalendarBuilders(
-                      todayDayBuilder: (context, date, events) => Container(
-                          padding: const EdgeInsets.all(4.0),
-                          alignment: Alignment.center,
-                          child: Text(
-                            date.day.toString(),
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Montserrat',
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      weekendDayBuilder: (context, date, events) => Container(
-                          padding: const EdgeInsets.all(4.0),
-                          alignment: Alignment.center,
-                          child: Text(
-                            date.day.toString(),
-                            style: TextStyle(
+                    Column(
+                      children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(right: 5.0),
+                            child: IconButton(
+                              icon: Icon(Icons.add),
                               color: Colors.black,
-                              fontFamily: 'Montserrat',
-                              fontSize: 16,
-                            ),
-                          )),
-                      dayBuilder: (context, date, events) => Container(
-                          padding: const EdgeInsets.all(8.0),
-                          alignment: Alignment.center,
-                          child: Text(
-                            date.day.toString(),
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontFamily: 'Montserrat',
-                            ),
-                          )),
-                      selectedDayBuilder: (context, date, events) => Container(
+                              iconSize: 24,
+                              onPressed: _showAddDialog,
+                            )),
+                      ],
+                    )
+                  ]),
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              TableCalendar(
+                events: _events,
+                initialCalendarFormat: CalendarFormat.month,
+                availableGestures: AvailableGestures.horizontalSwipe,
+                calendarStyle: CalendarStyle(
+                  canEventMarkersOverflow: true,
+                ),
+                headerStyle: HeaderStyle(
+                    leftChevronPadding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width * 0.2),
+                    rightChevronPadding: EdgeInsets.only(
+                        right: MediaQuery.of(context).size.width * 0.2),
+                    titleTextStyle: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                    centerHeaderTitle: true,
+                    formatButtonVisible: false),
+                startingDayOfWeek: StartingDayOfWeek.monday,
+                onDaySelected: (date, events) {
+                  setState(() {
+                    _selectedEvents = events;
+                  });
+                },
+                builders: CalendarBuilders(
+                  todayDayBuilder: (context, date, events) => Container(
+                      padding: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Montserrat',
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  weekendDayBuilder: (context, date, events) => Container(
+                      padding: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontFamily: 'Montserrat',
+                          fontSize: 16,
+                        ),
+                      )),
+                  dayBuilder: (context, date, events) => Container(
+                      padding: const EdgeInsets.all(8.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Montserrat',
+                        ),
+                      )),
+                  selectedDayBuilder: (context, date, events) => Container(
+                      padding: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Montserrat',
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  outsideDayBuilder: (context, date, events) => Container(
+                      padding: const EdgeInsets.all(5.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        date.day.toString(),
+                        style: TextStyle(
+                            color: Colors.black45,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold),
+                      )),
+                  outsideWeekendDayBuilder: (context, date, events) =>
+                      Container(
                           padding: const EdgeInsets.all(4.0),
-                          alignment: Alignment.center,
-                          child: Text(
-                            date.day.toString(),
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Montserrat',
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      outsideDayBuilder: (context, date, events) => Container(
-                          padding: const EdgeInsets.all(5.0),
                           alignment: Alignment.center,
                           child: Text(
                             date.day.toString(),
@@ -170,34 +188,22 @@ class _CalendarState extends State<Calendar> {
                                 fontFamily: 'Montserrat',
                                 fontWeight: FontWeight.bold),
                           )),
-                      outsideWeekendDayBuilder: (context, date, events) =>
-                          Container(
-                              padding: const EdgeInsets.all(4.0),
-                              alignment: Alignment.center,
-                              child: Text(
-                                date.day.toString(),
-                                style: TextStyle(
-                                    color: Colors.black45,
-                                    fontFamily: 'Montserrat',
-                                    fontWeight: FontWeight.bold),
-                              )),
-                      dowWeekdayBuilder: (context, weekday) => Container(
-                          padding: const EdgeInsets.all(4.0),
-                          alignment: Alignment.center,
-                          child: Text(
-                            weekday.toString(),
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold),
-                          )),
-                    ),
-                    calendarController: _controller,
-                  ),
-                ],
+                  dowWeekdayBuilder: (context, weekday) => Container(
+                      padding: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        weekday.toString(),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold),
+                      )),
+                ),
+                calendarController: _controller,
               ),
             ],
-          )),
+          ),
+
           Expanded(
               child: SingleChildScrollView(
             child: Column(
@@ -208,10 +214,6 @@ class _CalendarState extends State<Calendar> {
                   height: MediaQuery.of(context).size.height * 0.051,
                   decoration: BoxDecoration(
                     color: Color(0xffedeefc),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
                     boxShadow: <BoxShadow>[
                       BoxShadow(
                         color: Colors.black26,
