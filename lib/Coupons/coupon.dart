ae3a6f2c68d8346f741a89de8b8b97734c5e0897
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-05-30 00:52:32 +0530

    coupon 1st part done

--- lib/Coupons/coupon.dart
+++ lib/Coupons/coupon.dart
@@ -11,6 +11,104 @@ class CouponData extends StatefulWidget {
 class _CouponDataState extends State<CouponData> {
   final Firestore _firestore = Firestore.instance;
   ScrollController scrollController = ScrollController();
+
+  Widget _couponList(BuildContext context, DocumentSnapshot document) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+    bool couponAvailable = true;
+    updateCount() async {
+      setState(() {
+        if (document['limit'] < 1) {
+          couponAvailable = false;
+        } else {
+          document.reference.updateData({'limit': document['limit'] - 1});
+          couponAvailable = true;
+        }
+      });
+    }
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: 0,
+        horizontal: screenWidth * 0.1,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.amberAccent[100],
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                document["name"],
+                style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    document['limit'].toString(),
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
+                  ),
+                  Text(
+                    " coupon left",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text(
+                    document['amount'],
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24),
+                  ),
+                  Text(
+                    document['percentage'] ? "%" : "Rs",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ],
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.black,
+                ),
+                onPressed: () => updateCount(),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,152 +121,25 @@ class _CouponDataState extends State<CouponData> {
         children: <Widget>[
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
-              stream:
-                  _firestore.collection('coupons').orderBy('limit').snapshots(),
+              stream: _firestore.collection('coupons').snapshots(),
               builder: (context, snapshot) {
                 if (!snapshot.hasData)
                   return Center(
                     child: CircularProgressIndicator(),
                   );
-                List<DocumentSnapshot> docs = snapshot.data.documents;
-                List<Widget> coupons = docs
-                    .map((doc) => Coupon(
-                          amount: doc.data['amount'],
-                          name: doc.data['name'],
-                          percentage: doc.data['percentage'],
-                          limit: doc.data['limit'],
-                          // id: doc.data['id'],
-                        ))
-                    .toList();
-                return ListView(
+
+                return ListView.builder(
+                  itemExtent: 80,
+                  itemCount: snapshot.data.documents.length,
                   controller: scrollController,
                   // reverse: true,
-                  children: <Widget>[
-                    ...coupons,
-                  ],
+                  itemBuilder: (context, index) =>
+                      _couponList(context, snapshot.data.documents[index]),
                 );
               },
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class Coupon extends StatefulWidget {
-  final String amount, name;
-  final bool percentage;
-  final int limit;
-
-  const Coupon({Key key, this.amount, this.name, this.percentage, this.limit})
-      : super(key: key);
-
-  @override
-  _CouponState createState() => _CouponState();
-}
-
-class _CouponState extends State<Coupon> {
-  DocumentReference documentReference =
-      Firestore.instance.collection("coupons").document();
-  @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        vertical: screenHeight * 0.01,
-        horizontal: screenWidth * 0.1,
-      ),
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          vertical: screenHeight * 0.01,
-          horizontal: screenWidth * 0.1,
-        ),
-        constraints: BoxConstraints(
-          minHeight: screenHeight * 0.12,
-        ),
-        width: screenWidth * 0.9,
-        decoration: BoxDecoration(
-            color: Colors.amberAccent[100],
-            borderRadius: BorderRadius.all(Radius.circular(10))),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  widget.name,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                ),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "${widget.limit}",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14),
-                    ),
-                    Text(
-                      " coupon left",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14),
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      widget.amount,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24),
-                    ),
-                    Text(
-                      widget.percentage ? "%" : "Rs",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
-                  ],
-                ),
-                IconButton(
-                    icon: Icon(
-                      Icons.add,
-                      color: Colors.black,
-                    ),
-                    onPressed: () async {
-                      setState(() {
-                        Firestore.instance
-                            .collection("Coupon")
-                            .document(documentReference.documentID)
-                            .updateData({"limit": widget.limit - 1});
-                      });
-                    }),
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
