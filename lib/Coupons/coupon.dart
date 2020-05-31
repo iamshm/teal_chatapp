f23c596b591d5331869b85c2f670268a9831e119
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-05-31 23:47:45 +0530

    coupon required done with decrementing limit

--- lib/Coupons/coupon.dart
+++ lib/Coupons/coupon.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app/Coupons/SearchService.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -10,33 +9,26 @@ class CouponData extends StatefulWidget {
 }
 
 class _CouponDataState extends State<CouponData> {
-  var queryResultSet = [];
-  var tempSearchStore = [];
+  bool validCoupon = false;
+  TextEditingController _controller;
 
-  initiateSearch(value) {
+  validity(value) {
     if (value.length == 0) {
       setState(() {
-        queryResultSet = [];
-        tempSearchStore = [];
-      });
-    }
-
-    var capitalizedValue =
-        value.substring(0, 1).toUpperCase() + value.substring(1);
-
-    if (queryResultSet.length == 0 && value.length == 1) {
-      SearchService().searchByName(value).then((QuerySnapshot docs) {
-        for (int i = 0; i < docs.documents.length; ++i) {
-          queryResultSet.add(docs.documents[i].data);
-        }
+        validCoupon = false;
       });
     } else {
-      tempSearchStore = [];
-      queryResultSet.forEach((element) {
-        if (element['name'].startsWith(capitalizedValue)) {
+      Firestore.instance
+          .collection("coupons")
+          .where('name', isEqualTo: value)
+          .getDocuments()
+          .then((QuerySnapshot docs) {
+        if (docs.documents.isNotEmpty) {
           setState(() {
-            tempSearchStore.add(element);
+            validCoupon = true;
           });
+          docs.documents[0].reference
+              .updateData({'limit': docs.documents[0].data['limit'] - 1});
         }
       });
     }
@@ -48,161 +40,29 @@ class _CouponDataState extends State<CouponData> {
         appBar: new AppBar(
           title: Text('Coupons '),
         ),
-        body: ListView(children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              onChanged: (val) {
-                initiateSearch(val);
-              },
-              decoration: InputDecoration(
-                  prefixIcon: IconButton(
-                    color: Colors.black,
-                    icon: Icon(Icons.arrow_back),
-                    iconSize: 20.0,
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  contentPadding: EdgeInsets.only(left: 25.0),
-                  hintText: 'Search available coupons',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(4.0))),
-            ),
-          ),
-          SizedBox(height: 10.0),
-          GridView.count(
-              padding: EdgeInsets.only(left: 10.0, right: 10.0),
-              crossAxisCount: 2,
-              crossAxisSpacing: 4.0,
-              mainAxisSpacing: 4.0,
-              primary: false,
-              shrinkWrap: true,
-              children: tempSearchStore.map((element) {
-                return buildResultCard(element);
-              }).toList())
-        ]));
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  onChanged: (val) {
+                    validity(val);
+                  },
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 25.0),
+                      hintText: 'Enter coupon',
+                      hintStyle: TextStyle(),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(4.0))),
+                ),
+              ),
+              Text(
+                validCoupon
+                    ? "Coupon applied Successfully"
+                    : "Enter a valid Coupon",
+                style: TextStyle(fontSize: 24),
+              ),
+            ]));
   }
-}
-
-Widget buildResultCard(data) {
-  return Card(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-    elevation: 2.0,
-    child: Container(
-        color: Colors.green[200],
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  data['name'],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.add,
-                      size: 24,
-                      color: Colors.red,
-                    ),
-                    onPressed: () => {},
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  data['amount'],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  data['percentage'] ? " % discount" : " Rs Off",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  " Expires on ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  "DateOfExpiry",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  " Can be used ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  data['limit'].toString(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 20,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  " times ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 12,
-                      fontFamily: 'Montserrat',
-                      fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ],
-        )),
-  );
 }
