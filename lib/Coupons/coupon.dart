f69d9b29c0356eae1d87b5db20d31bd789311baf
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-06-01 16:19:00 +0530

    coupon with per use

--- lib/Coupons/coupon.dart
+++ lib/Coupons/coupon.dart
@@ -10,7 +10,8 @@ class CouponData extends StatefulWidget {
 
 class _CouponDataState extends State<CouponData> {
   bool validCoupon = false;
-  TextEditingController _controller;
+
+  DocumentSnapshot docref;
 
   validity(value) {
     if (value.length == 0) {
@@ -27,42 +28,74 @@ class _CouponDataState extends State<CouponData> {
           setState(() {
             validCoupon = true;
           });
-          docs.documents[0].reference
-              .updateData({'limit': docs.documents[0].data['limit'] - 1});
+          docref = docs.documents[0];
+        } else {
+          setState(() {
+            validCoupon = false;
+          });
         }
       });
+    }
+  }
+
+  useCoupon() {
+    if (validCoupon) {
+      if (docref.data['limit'] > 0 && docref.data['perUserLimit'] > 0) {
+        docref.reference.updateData({'limit': docref.data['limit'] - 1});
+        docref.reference
+            .updateData({'perUserLimit': docref.data['perUserLimit'] - 1});
+      } else {
+        validCoupon = false;
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: Text('Coupons '),
-        ),
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextField(
-                  onChanged: (val) {
-                    validity(val);
-                  },
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.only(left: 25.0),
-                      hintText: 'Enter coupon',
-                      hintStyle: TextStyle(),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(4.0))),
+      appBar: new AppBar(
+        title: Text('Coupons '),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              onSubmitted: (value) => {validity(value)},
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(left: 25.0),
+                hintText: 'Enter coupon',
+                hintStyle: TextStyle(),
+                suffixIcon:
+                    IconButton(icon: Icon(Icons.send), onPressed: useCoupon()),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(4.0),
                 ),
               ),
+            ),
+          ),
+          Text(
+            validCoupon ? "Coupon is Valid" : "Enter a valid Coupon",
+            style: TextStyle(fontSize: 24),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
               Text(
                 validCoupon
-                    ? "Coupon applied Successfully"
-                    : "Enter a valid Coupon",
+                    ? (docref.data["limit"] - 1).toString()
+                    : "Not a valid",
                 style: TextStyle(fontSize: 24),
               ),
-            ]));
+              Text(
+                " Coupon ",
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
