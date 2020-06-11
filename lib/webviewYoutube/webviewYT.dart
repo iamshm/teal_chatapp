388bf15304fdafac3acaec6dddf34396aa766ebd
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-06-11 21:29:05 +0530

    ytupdated

--- lib/webviewYoutube/webviewYT.dart
+++ lib/webviewYoutube/webviewYT.dart
@@ -11,28 +11,52 @@ class WebViewYoutube extends StatefulWidget {
 
 class _WebViewYoutubeState extends State<WebViewYoutube> {
   String uri = "https://www.youtube.com/embed/5VbAwhBBHsg";
-  WebViewController _controller;
+  WebViewController _myController;
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-        height: MediaQuery.of(context).size.height,
-        child: WebView(
-            initialUrl: Uri.dataFromString(
-                    '<html><body><iframe width="440" height="315" src="https://www.youtube.com/embed/5VbAwhBBHsg?rel=0&controls=0&color=white&modestbranding=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></body></html>',
-                    mimeType: 'text/html')
-                .toString(),
-            javascriptMode: JavascriptMode.unrestricted,
-            onPageFinished: (uri) {
-              _controller.evaluateJavascript(
-                  "document.getElementsByClassName('ytp-youtube-button ytp-button yt-uix-sessionlink').style.display='none';"
-                  "console.log(document.querySelectorAll('ytp-youtube-button ytp-button yt-uix-sessionlink'));");
-            }
+      body: SafeArea(
+        child: Center(
+            child: Container(
+          child: WebView(
+              initialUrl: Uri.dataFromString(
+                      '<html><body><iframe width="440" height="315" src="https://www.youtube.com/embed/5VbAwhBBHsg?rel=0&controls=0&color=red&modestbranding=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></body></html>',
+                      mimeType: 'text/html')
+                  .toString(),
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controller.complete(webViewController);
+                _myController = webViewController;
+              },
+              onPageFinished: (uri) async {
+                String result1 =
+                    await _myController.evaluateJavascript("10 + 20;");
+                print(result1); // 30
 
-            //  modestbranding="1"
-            ),
-      )),
+                var result2 = await _myController.evaluateJavascript("""
+                      var allClasses = [];
+
+                      var allElements = document.querySelectorAll('*');
+
+                      for (var i = 0; i < allElements.length; i++) {
+                        var classes = allElements[i].className.toString().split(/\s+/);
+                        for (var j = 0; j < classes.length; j++) {
+                          var cls = classes[j];
+                          if (cls && allClasses.indexOf(cls) === -1)
+                            allClasses.push(cls);
+                        }
+                      }
+
+                      return (allClasses);
+
+                      """);
+                print(result2);
+              }),
+        )),
+      ),
     );
   }
 }
