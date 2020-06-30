edce0169cff1fe77f3eed94b53daa3c4ee6cbbeb
Suraj Maharana <43462941+iamshm@users.noreply.github.com>
2020-06-30 16:48:16 +0530

    dynamic

--- lib/horizontalOptions/horizontalList.dart
+++ lib/horizontalOptions/horizontalList.dart
@@ -6,6 +6,13 @@ class HorizontalList extends StatelessWidget {
   static const String id = "HORIZONTALLIST";
   @override
   Widget build(BuildContext context) {
+    List<double> posY = [50, 70, 250, 200, 350];
+
+    List<double> posX = [50, 230, 20, 170, 220];
+
+    int dataLength = 7;
+    int tempLength = (dataLength - dataLength % 5) ~/ 5 + 1;
+
     return Scaffold(
       backgroundColor: Color(0xffe2f5fb),
       body: SafeArea(
@@ -36,200 +43,63 @@ class HorizontalList extends StatelessWidget {
           Container(
             height: 600,
             width: MediaQuery.of(context).size.width,
-            child: ListView(
+            child: ListView.builder(
               scrollDirection: Axis.horizontal,
               shrinkWrap: true,
-              children: <Widget>[
-                Container(
-                  height: 600,
-                  width: MediaQuery.of(context).size.width,
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                        top: 50,
-                        left: 50,
-                        child: OptionButton(
-                          optionName: "Food",
-                          optionIcon: FontAwesomeIcons.pizzaSlice,
-                          colorFul: true,
-                          optionHeight: 140,
-                          optionWidth: 140,
-                        ),
-                      ),
-                      Positioned(
-                        top: 70,
-                        right: 70,
-                        child: OptionButton(
-                          optionName: "Retail",
-                          optionIcon: FontAwesomeIcons.cartPlus,
-                          colorFul: false,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 250,
-                        left: 20,
-                        child: OptionButton(
-                          optionName: "Travel",
-                          optionIcon: FontAwesomeIcons.globeAmericas,
-                          colorFul: false,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 200,
-                        right: 120,
-                        child: OptionButton(
-                          optionName: "Sports",
-                          optionIcon: FontAwesomeIcons.dumbbell,
-                          colorFul: true,
-                          optionHeight: 130,
-                          optionWidth: 130,
-                        ),
-                      ),
-                      Positioned(
-                        top: 350,
-                        right: 50,
-                        child: OptionButton(
-                          optionName: "Entertainment",
-                          optionIcon: FontAwesomeIcons.music,
-                          colorFul: false,
-                          optionHeight: 150,
-                          optionWidth: 150,
-                        ),
-                      ),
-                    ],
-                  ),
+              itemCount: tempLength,
+              itemBuilder: (context, i) => Container(
+                height: 600,
+                width: MediaQuery.of(context).size.width,
+                child: Stack(
+                  children: <Widget>[
+                    OptionButton(
+                      topD: posY[0],
+                      leftD: posX[0],
+                      optionName: "Food",
+                      optionIcon: FontAwesomeIcons.pizzaSlice,
+                      colorFul: true,
+                      optionHeight: 140,
+                      optionWidth: 140,
+                    ),
+                    OptionButton(
+                      topD: posY[1],
+                      leftD: posX[1],
+                      optionName: "Retail",
+                      optionIcon: FontAwesomeIcons.cartPlus,
+                      colorFul: false,
+                      optionHeight: 120,
+                      optionWidth: 120,
+                    ),
+                    OptionButton(
+                      topD: posY[2],
+                      leftD: posX[2],
+                      optionName: "Travel",
+                      optionIcon: FontAwesomeIcons.globeAmericas,
+                      colorFul: false,
+                      optionHeight: 130,
+                      optionWidth: 130,
+                    ),
+                    OptionButton(
+                      topD: posY[3],
+                      leftD: posX[3],
+                      optionName: "Sports",
+                      optionIcon: FontAwesomeIcons.dumbbell,
+                      colorFul: true,
+                      optionHeight: 130,
+                      optionWidth: 130,
+                    ),
+                    OptionButton(
+                      topD: posY[4],
+                      leftD: posX[4],
+                      optionName: "Entertainment",
+                      optionIcon: FontAwesomeIcons.music,
+                      colorFul: false,
+                      optionHeight: 150,
+                      optionWidth: 150,
+                    ),
+                  ],
                 ),
-                Container(
-                  height: 600,
-                  width: MediaQuery.of(context).size.width,
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                        top: 50,
-                        left: 10,
-                        child: OptionButton(
-                          optionName: "Food",
-                          optionIcon: FontAwesomeIcons.pizzaSlice,
-                          colorFul: false,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 70,
-                        right: 70,
-                        child: OptionButton(
-                          optionName: "Retail",
-                          optionIcon: FontAwesomeIcons.cartPlus,
-                          colorFul: false,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 250,
-                        left: 20,
-                        child: OptionButton(
-                          optionName: "Travel",
-                          optionIcon: FontAwesomeIcons.globeAmericas,
-                          colorFul: false,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 200,
-                        right: 120,
-                        child: OptionButton(
-                          optionName: "Sports",
-                          optionIcon: FontAwesomeIcons.dumbbell,
-                          colorFul: true,
-                          optionHeight: 130,
-                          optionWidth: 130,
-                        ),
-                      ),
-                      Positioned(
-                        top: 350,
-                        right: 50,
-                        child: OptionButton(
-                          optionName: "Entertainment",
-                          optionIcon: FontAwesomeIcons.music,
-                          colorFul: false,
-                          optionHeight: 150,
-                          optionWidth: 150,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 600,
-                  width: MediaQuery.of(context).size.width,
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned(
-                        top: 50,
-                        left: 10,
-                        child: OptionButton(
-                          optionName: "Food",
-                          optionIcon: FontAwesomeIcons.pizzaSlice,
-                          colorFul: true,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 70,
-                        right: 70,
-                        child: OptionButton(
-                          optionName: "Retail",
-                          optionIcon: FontAwesomeIcons.cartPlus,
-                          colorFul: true,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 250,
-                        left: 20,
-                        child: OptionButton(
-                          optionName: "Travel",
-                          optionIcon: FontAwesomeIcons.globeAmericas,
-                          colorFul: false,
-                          optionHeight: 120,
-                          optionWidth: 120,
-                        ),
-                      ),
-                      Positioned(
-                        top: 200,
-                        right: 120,
-                        child: OptionButton(
-                          optionName: "Sports",
-                          optionIcon: FontAwesomeIcons.dumbbell,
-                          colorFul: false,
-                          optionHeight: 130,
-                          optionWidth: 130,
-                        ),
-                      ),
-                      Positioned(
-                        top: 350,
-                        right: 150,
-                        child: OptionButton(
-                          optionName: "Entertainment",
-                          optionIcon: FontAwesomeIcons.music,
-                          colorFul: true,
-                          optionHeight: 150,
-                          optionWidth: 150,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
           )
         ],
@@ -237,124 +107,3 @@ class HorizontalList extends StatelessWidget {
     );
   }
 }
-
-//  Positioned(
-//                         top: 50,
-//                         left: 50,
-//                         child: OptionButton(
-//                           optionName: "Food",
-//                           optionIcon: FontAwesomeIcons.pizzaSlice,
-//                           colorFul: true,
-//                           optionHeight: 140,
-//                           optionWidth: 140,
-//                         ),
-//                       ),
-//                       Positioned(
-//                         top: 70,
-//                         right: 70,
-//                         child:
-
-//                       Positioned(
-//                         top: 250,
-//                         left: 20,
-//                         child:
-
-//                       ),
-//                       Positioned(
-//                         top: 200,
-//                         right: 120,
-//                         child: OptionButton(
-//                           optionName: "Sports",
-//                           optionIcon: FontAwesomeIcons.dumbbell,
-//                           colorFul: true,
-//                           optionHeight: 130,
-//                           optionWidth: 130,
-//                         ),
-//                       ),
-//                       Positioned(
-//                         top: 350,
-//                         right: 50,
-//                         child: OptionButton(
-//                           optionName: "Entertainment",
-//                           optionIcon: FontAwesomeIcons.music,
-//                           colorFul: false,
-//                           optionHeight: 150,
-//                           optionWidth: 150,
-//                         ),
-//                       ),
-
-//  Column(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                     children: <Widget>[
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                         children: <Widget>[
-//                           SizedBox(
-//                             width: 40,
-//                           ),
-//                           OptionButton(
-//                             optionName: "Food",
-//                             optionIcon: FontAwesomeIcons.pizzaSlice,
-//                             colorFul: true,
-//                             optionHeight: 140,
-//                             optionWidth: 140,
-//                           ),
-//                           SizedBox(
-//                             width: 20,
-//                           ),
-//                           OptionButton(
-//                             optionName: "Retail",
-//                             optionIcon: FontAwesomeIcons.cartPlus,
-//                             colorFul: false,
-//                             optionHeight: 120,
-//                             optionWidth: 120,
-//                           ),
-//                         ],
-//                       ),
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                         crossAxisAlignment: CrossAxisAlignment.start,
-//                         children: <Widget>[
-//                           SizedBox(
-//                             width: 50,
-//                           ),
-//                           OptionButton(
-//                             optionName: "Travel",
-//                             optionIcon: FontAwesomeIcons.globeAmericas,
-//                             colorFul: false,
-//                             optionHeight: 130,
-//                             optionWidth: 130,
-//                           ),
-//                           SizedBox(
-//                             width: 50,
-//                           ),
-//                           OptionButton(
-//                             optionName: "Food",
-//                             optionIcon: FontAwesomeIcons.pizzaSlice,
-//                             colorFul: true,
-//                             optionHeight: 140,
-//                             optionWidth: 140,
-//                           ),
-//                         ],
-//                       ),
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                         children: <Widget>[
-//                           OptionButton(
-//                             optionName: "Food",
-//                             optionIcon: FontAwesomeIcons.pizzaSlice,
-//                             colorFul: true,
-//                             optionHeight: 140,
-//                             optionWidth: 140,
-//                           ),
-//                           OptionButton(
-//                             optionName: "Food",
-//                             optionIcon: FontAwesomeIcons.pizzaSlice,
-//                             colorFul: true,
-//                             optionHeight: 140,
-//                             optionWidth: 140,
-//                           ),
-//                         ],
-//                       ),
-//                     ],
-//                   ),
--- lib/horizontalOptions/optionButton.dart
+++ lib/horizontalOptions/optionButton.dart
@@ -6,6 +6,8 @@ class OptionButton extends StatelessWidget {
   final bool colorFul;
   final double optionHeight;
   final double optionWidth;
+  final double topD;
+  final double leftD;
 
   const OptionButton({
     Key key,
@@ -14,44 +16,50 @@ class OptionButton extends StatelessWidget {
     this.colorFul,
     this.optionHeight,
     this.optionWidth,
+    this.topD,
+    this.leftD,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(8),
-      child: Container(
-        height: optionHeight,
-        width: optionWidth,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              stops: [0.2, 1],
-              colors: colorFul
-                  ? [Color(0xff18dbcb), Colors.blue]
-                  : [Colors.white30, Colors.grey[350]]),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-                icon: Icon(
-                  optionIcon,
+    return Positioned(
+      top: topD,
+      left: leftD,
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Container(
+          height: optionHeight,
+          width: optionWidth,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.2, 1],
+                colors: colorFul
+                    ? [Color(0xff18dbcb), Colors.blue]
+                    : [Colors.white30, Colors.grey[350]]),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                  icon: Icon(
+                    optionIcon,
+                    color: colorFul ? Colors.white : Colors.black87,
+                  ),
+                  onPressed: null),
+              Text(
+                optionName,
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700,
                   color: colorFul ? Colors.white : Colors.black87,
                 ),
-                onPressed: null),
-            Text(
-              optionName,
-              style: TextStyle(
-                fontFamily: 'Montserrat',
-                fontSize: 15,
-                fontWeight: FontWeight.w700,
-                color: colorFul ? Colors.white : Colors.black87,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
